5d6d90e51f80f5be8359008c9c375390f0f39cdc
Akshay-appu <appu03626@gmail.com>
2025-11-29 05:11:52 +0530

    Update apps.py

--- monitoring/apps.py
+++ monitoring/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
 
+
 class MonitoringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'monitoring'
+
+    def ready(self):
+        # Auto-run migrations on startup (quick fix for Render)
+        from django.core.management import call_command
+        try:
+            call_command('migrate', interactive=False)
+        except Exception:
+            # If migrate fails for some reason, don't crash the app
+            pass
